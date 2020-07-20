208c20e835022a95a9ffc9749655009a09e6c350
13297960275 <13297960275@github.com>
2020-07-20 23:23:40 +0800

    project camare

--- uni-app/pages/projects/project.vue
+++ uni-app/pages/projects/project.vue
@@ -8,14 +8,17 @@
 				</button> -->
 			</view>
 		</title-item>
-		<view class="example-body">
+		<view class="uni-common-mt">
 			<view v-if="dynamicList.length!==0" class="grid-dynamic-box">
-				<uni-grid :column="3" :highlight="true" @change="change">
+				 <!-- @change="change" -->
+				<uni-grid :column="3" :highlight="true">
 					<uni-grid-item v-for="(item, index) in dynamicList" :index="index" :key="index">
+						<text class="time">{{item.time}}</text>
 						<view class="grid-item-box">
-							<image :src="item.url" class="image" mode="aspectFill" />
+							<image :src="item.url" class="image" mode="aspectFill" @click="perviewImg(item.urls)" />
 							<uni-icons :type="'camera'" :color="'#bbb'" size="25" @click="chooseImage(index)" />
 						</view>
+						<text class="text">{{item.text}}</text>
 					</uni-grid-item>
 				</uni-grid>
 			</view>
@@ -27,6 +30,7 @@
 
 <script>
 	import titleItem from './components/title-item';
+	import { dateUtils } from '@/common/util.js'
 	import {
 		pathToBase64,
 		base64ToPath,
@@ -39,51 +43,7 @@
 		data() {
 			return {
 				dynamicList: [],
-				list: [{
-						url: '/static/c1.png',
-						text: 'Grid 1',
-						badge: '0',
-						type: "primary"
-					},
-					{
-						url: '/static/c2.png',
-						text: 'Grid 2',
-						badge: '1',
-						type: "success"
-					},
-					{
-						url: '/static/c3.png',
-						text: 'Grid 3',
-						badge: '99',
-						type: "warning"
-					},
-					{
-						url: '/static/c4.png',
-						text: 'Grid 4',
-						badge: '2',
-						type: "error"
-					},
-					{
-						url: '/static/c5.png',
-						text: 'Grid 5'
-					},
-					{
-						url: '/static/c6.png',
-						text: 'Grid 6'
-					},
-					{
-						url: '/static/c7.png',
-						text: 'Grid 7'
-					},
-					{
-						url: '/static/c8.png',
-						text: 'Grid 8'
-					},
-					{
-						url: '/static/c9.png',
-						text: 'Grid 9'
-					}
-				]
+				list: []
 			}
 		},
 		methods: {
@@ -91,12 +51,20 @@
 				let {
 					index
 				} = e.detail
+				console.log(index, e)
+				return;
 				this.list[index].badge && this.list[index].badge++
 
 				uni.showToast({
 					title: `点击第${index+1}个宫格`,
 					icon: 'none'
 				})
+			},
+			perviewImg(urls) {
+				console.log('previewImg');
+				uni.previewImage({
+					urls: urls
+				});
 			},
 			chooseImage(index) {
 				console.log('chooseImage');
@@ -106,6 +74,8 @@
 					sourceType: ['album', 'camera '], //album 从相册选图，camera 使用相机，默认二者都有
 					success: (chooseImageRes) => {
 						for (var i = 0; i < chooseImageRes.tempFilePaths.length; i++) {
+							this.dynamicList[index].urls = []
+							this.dynamicList[index].urls.push(chooseImageRes.tempFilePaths[i]);
 							pathToBase64(chooseImageRes.tempFilePaths[i]).then(res => {
 								// console.log(res);
 								let size = getImgSize(res)
@@ -143,7 +113,9 @@
 				if (this.dynamicList.length < 9) {
 					this.dynamicList.push({
 						url: `/static/c${this.dynamicList.length+1}.png`,
-						text: `Grid ${this.dynamicList.length+1}`,
+						// time: dateUtils.format(new Date()),
+						// time: new Date(),
+						text: this.dynamicList.length + 1 % 3 === 1 ? '进场' : this.dynamicList.length + 1 % 3 === 2 ? '施工' : "结束",
 						color: this.dynamicList.length % 2 === 0 ? '#f5f5f5' : "#fff"
 					})
 				} else {
@@ -161,106 +133,6 @@
 </script>
 
 <style>
-	/* 头条小程序组件内不能引入字体 */
-	/* #ifdef MP-TOUTIAO */
-	@font-face {
-		font-family: uniicons;
-		font-weight: normal;
-		font-style: normal;
-		src: url('~@/static/uni.ttf') format('truetype');
-	}
-
-	/* #endif */
-
-	/* #ifndef APP-NVUE */
-	page {
-		display: flex;
-		flex-direction: column;
-		box-sizing: border-box;
-		background-color: #efeff4;
-		min-height: 100%;
-		height: auto;
-	}
-
-	view {
-		font-size: 14px;
-		line-height: inherit;
-	}
-
-	.example {
-		padding: 0 15px 15px;
-	}
-
-	.example-info {
-		padding: 15px;
-		color: #3b4144;
-		background: #ffffff;
-	}
-
-	.example-body {
-		/* #ifndef APP-NVUE */
-		display: flex;
-		/* #endif */
-		flex-direction: row;
-		flex-wrap: wrap;
-		justify-content: center;
-		padding: 0;
-		font-size: 14px;
-		background-color: #ffffff;
-	}
-
-	/* #endif */
-	.example {
-		padding: 0 15px;
-	}
-
-	.example-info {
-		/* #ifndef APP-NVUE */
-		display: block;
-		/* #endif */
-		padding: 15px;
-		color: #3b4144;
-		background-color: #ffffff;
-		font-size: 14px;
-		line-height: 20px;
-	}
-
-	.example-info-text {
-		font-size: 14px;
-		line-height: 20px;
-		color: #3b4144;
-	}
-
-
-	.example-body {
-		flex-direction: column;
-		padding: 15px;
-		background-color: #ffffff;
-	}
-
-	.word-btn-white {
-		font-size: 18px;
-		color: #FFFFFF;
-	}
-
-	.word-btn {
-		/* #ifndef APP-NVUE */
-		display: flex;
-		/* #endif */
-		flex-direction: row;
-		align-items: center;
-		justify-content: center;
-		border-radius: 6px;
-		height: 48px;
-		margin: 15px;
-		background-color: #21caad;
-	}
-
-	.word-btn--hover {
-		background-color: #4ca2ff;
-	}
-
-
 	.image {
 		width: 120rpx;
 		height: 120rpx;
@@ -268,13 +140,11 @@
 
 	.text {
 		font-size: 26rpx;
-		margin-top: 10rpx;
-	}
-
-	.example-body {
-		/* #ifndef APP-NVUE */
-		display: block;
-		/* #endif */
+		/* margin-top: 10rpx; */
+		text-align: center;
+		background-color: #eee;
+		padding: 10rpx;
+		margin: 10rpx;
 	}
 
 	.grid-dynamic-box {
@@ -283,8 +153,7 @@
 
 	.grid-item-box {
 		flex: 1;
-		/* position: relative;
- */
+		/* position: relative; */
 		/* #ifndef APP-NVUE */
 		display: flex;
 		/* #endif */
@@ -293,15 +162,5 @@
 		/* justify-content: center; */
 		justify-content: space-around;
 		padding: 15px 0;
-	}
-
-	.grid-dot {
-		position: absolute;
-		top: 5px;
-		right: 15px;
-	}
-
-	.swiper {
-		height: 420px;
 	}
 </style>
